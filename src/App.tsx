108961de98d5364d911c77f0a113e0a7f2288b07
KaeTheDev <kaethedev@gmail.com>
2025-12-14 18:44:40 -0500

    Added hardcoded layout for todo app in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 function App() {
   return (
-    <>
-    <h2 className="text-3xl text-blue-500">KaeTheDev is the GREATEST!</h2>
-    </>
+<div className="flex flex-col px-4 py-4 mx-auto bg-black p-4 border rounded max-w-2xl mt-4 items-center">
+   <div className="flex flex-row">
+   <h1 className="text-2xl text-white">Todo App(Context API)</h1>
+   <button className="bg-yellow-300 px-4 py-5 ml-6">☀️ Switch to Light</button>
+   </div>
+   <div className="flex flex-row mt-4">
+   <input className="border px-4 py-4 mr-4 text-white" type="text" placeholder="What needs to be done?" />
+   <button className="bg-blue-600 px-3 py-4 hover:bg-blue-400">Add Todo</button>
+   </div>
+
+  <div className="flex flex-row mt-4">
+    <button className="bg-gray-700 text-white px-4 py-5 mr-4">All</button>
+    <button className="bg-gray-700 text-white px-4 py-5 mr-4">Active</button>
+    <button className="bg-gray-700 text-white px-4 py-5">Completed</button>
+  </div>
+</div>
   )
 }
 
