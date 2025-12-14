7bb52716072a0c1ab442e11362bab49d721e25d8
KaeTheDev <kaethedev@gmail.com>
2025-12-14 17:29:28 -0500

    Initial Project setup with tailwindcss and folders and files

---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+export interface Todo {
+    id: string,
+    text: string,
+    completed: boolean;
+}
+
+export interface TodoContextType {
+    todos: Todo[];
+    addTodo: (text: string) => void;
+    toggleTodo: (id: string) => void;
+    deleteTodo: (id: string) => void;
+    editTodo: (id: string, newText: string) => void;
+    clearCompleted: () => void;
+}
